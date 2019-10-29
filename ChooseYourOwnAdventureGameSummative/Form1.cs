b7117cdde7e6bc7b635b5d4fef3220794cfdd135
sridpate869 <sridpate869@SNS-D-130S0C>
2019-10-29 13:16:12 -0400

    Started Code

--- ChooseYourOwnAdventureGameSummative/Form1.cs
+++ ChooseYourOwnAdventureGameSummative/Form1.cs
@@ -7,14 +7,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace ChooseYourOwnAdventureGameSummative
 {
     public partial class Form1 : Form
     {
+
+        int scene = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.M)       //red button press
+            {
+                if (scene == 0) { scene = ; }
+                else if (scene == 1) { scene =  }
+                else if (scene == 2) { scene =  }
+                else if (scene == 3) { scene =  }
+                else if (scene == 4) { scene =  }
+                else if (scene == 5) { scene =  }
+                else if (scene == 6) { scene =  }
+                else if (scene == 7) { scene =  }
+                else if (scene == 8) { scene =  }
+                else if (scene == 9) { scene =  }
+                else if (scene == 10) { scene =  }
+                else if (scene == 11) { scene =  }
+                else if (scene == 12) { scene =  }
+                else if (scene == 13) { scene =  }
+                else if (scene == 14) { scene =  }
+                else if (scene == 15) { scene =  }
+                else if (scene == 16) { scene =  }
+                else if (scene == 17) { scene =  }
+            }
+            else if (e.KeyCode == Keys.B)  //Green button press
+            {
+                if (scene == 0) { scene = 1; }
+                else if (scene == 1) { scene =  }
+                else if (scene == 2) { scene =  }
+                else if (scene == 3) { scene =  }
+                else if (scene == 4) { scene =  }
+                else if (scene == 5) { scene =  }
+                else if (scene == 6) { scene =  }
+                else if (scene == 7) { scene =  }
+                else if (scene == 8) { scene =  }
+                else if (scene == 9) { scene =  }
+                else if (scene == 10) { scene =  }
+                else if (scene == 11) { scene =  }
+                else if (scene == 12) { scene =  }
+                else if (scene == 13) { scene =  }
+                else if (scene == 14) { scene =  }
+                else if (scene == 15) { scene =  }
+                else if (scene == 16) { scene =  }
+                else if (scene == 17) { scene =  }
+            }
         }
     }
 }
