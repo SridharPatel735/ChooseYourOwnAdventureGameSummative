61795250afdc6a908a30f794b9fa5ea6c635c80f
sridpate869 <sridpate869@SNS-D-130S0C>
2019-10-30 13:27:50 -0400

    finished if else

--- ChooseYourOwnAdventureGameSummative/Form1.Designer.cs
+++ ChooseYourOwnAdventureGameSummative/Form1.Designer.cs
@@ -30,22 +30,32 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.outputLabel = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // outputLabel
             // 
-            this.outputLabel.AutoSize = true;
-            this.outputLabel.Location = new System.Drawing.Point(215, 116);
+            this.outputLabel.Location = new System.Drawing.Point(167, 216);
             this.outputLabel.Name = "outputLabel";
-            this.outputLabel.Size = new System.Drawing.Size(35, 13);
+            this.outputLabel.Size = new System.Drawing.Size(252, 108);
             this.outputLabel.TabIndex = 0;
             this.outputLabel.Text = "label1";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Location = new System.Drawing.Point(219, 93);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(33, 13);
+            this.scoreLabel.TabIndex = 1;
+            this.scoreLabel.Text = "score";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(569, 440);
+            this.Controls.Add(this.scoreLabel);
             this.Controls.Add(this.outputLabel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
@@ -59,6 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label scoreLabel;
     }
 }
 
--- ChooseYourOwnAdventureGameSummative/Form1.cs
+++ ChooseYourOwnAdventureGameSummative/Form1.cs
@@ -21,11 +21,19 @@ namespace ChooseYourOwnAdventureGameSummative
         public Form1()
         {
             InitializeComponent();
+            scoreLabel.Text = "" + scene;
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            outputLabel.Text = "" + scene;
+            if (scene == 2) { scene = 97; }
+            else if (scene == 4) { scene = 97; }
+            else if (scene == 6) { scene = 97; }
+            else if (scene == 9) { scene = 97; }
+            else if (scene == 10) { scene = 97; }
+            else if (scene == 13) { scene = 97; }
+            else if (scene == 17) { scene = 97; }
+
             if (e.KeyCode == Keys.M)       //red button press
             {
                 if (scene == 0) { scene = 11; }
@@ -35,14 +43,12 @@ namespace ChooseYourOwnAdventureGameSummative
                     if (percent >= 50) { scene = 2; }
                     else { scene = 3; }
                 }
-                else if (scene == 2) { scene = 98; }
                 else if (scene == 3)
                 {
                     percent = randGen.Next(0, 101);
                     if (percent >= 25) { scene = 4; }
                     else { scene = 5; }
                 }
-                else if (scene == 4) { scene = 98; }
                 else if (scene == 5) { scene = 8; }
                 else if (scene == 8)
                 {
@@ -50,25 +56,23 @@ namespace ChooseYourOwnAdventureGameSummative
                     if (percent >= 10) { scene = 9; }
                     else { scene = 0; }
                 }
-                else if (scene == 9) { scene = 98; }
                 else if (scene == 11) { scene = 12; }
-                // if (scene == 12) { scene =  }
-                else if (scene == 13) { scene = 98; }
+                else if (scene == 12) { scene = 13; }
                 else if (scene == 14) { scene = 13; }
                 else if (scene == 15) { scene = 13; }
+                else if (scene == 97) { scene = 0; }
+                else if (scene == 99) { scene = 0; }
             }
             else if (e.KeyCode == Keys.B)  //Green button press
             {
                 if (scene == 0) { scene = 1; }
                 else if (scene == 1) { scene = 0; }
-                else if (scene == 2) { scene = 98; }
                 else if (scene == 3)
                 {
                     percent = randGen.Next(0, 101);
-                    if (percent >= 25) {scene = 6; }
-                    else {scene = 7; }
+                    if (percent >= 25) { scene = 6; }
+                    else { scene = 7; }
                 }
-                else if (scene == 6) { scene = 98; }
                 else if (scene == 7) { scene = 8; }
                 else if (scene == 8)
                 {
@@ -76,10 +80,8 @@ namespace ChooseYourOwnAdventureGameSummative
                     if (percent >= 10) { scene = 10; }
                     else { scene = 99; }
                 }
-                else if (scene == 10) { scene = 98; }
                 else if (scene == 11) { scene = 14; }
-                //else if (scene == 12) { scene =   }
-                else if (scene == 13) { scene = 98; }
+                else if (scene == 12) { scene = 14; }
                 else if (scene == 14) { scene = 1; }
                 else if (scene == 15)
                 {
@@ -88,7 +90,69 @@ namespace ChooseYourOwnAdventureGameSummative
                     else { scene = 16; }
                 }
                 else if (scene == 16) { scene = 99; }
-                else if (scene == 17) { scene = 98; }
+                else if (scene == 97) { scene = 98; }
+                else if (scene == 99) { scene = 97; }
+            }
+            else if (e.KeyCode == Keys.Space)  //Blue Button
+            {
+                if (scene == 12) { scene = 14; }
+                else if (scene == 14) { scene = 15; }
+            }
+            else if (e.KeyCode == Keys.N)  //Yellow Button
+            {
+                if (scene == 12) { scene = 13; }
+            }
+
+            scoreLabel.Text = "" + scene;
+
+            switch (scene)
+            {
+                case 0:  //start scene  
+                    outputLabel.Text = "You find yourself at the edge of a forest. You hear a noise inside the forest. " +
+                        "On the other side of the forest is a road.";
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    break;
+                case 12:
+                    break;
+                case 13:
+                    break;
+                case 14:
+                    break;
+                case 15:
+                    break;
+                case 16:
+                    break;
+                case 17:
+                    break;
+                case 97:
+                    break;
+                case 98:
+                    break;
+                case 99:
+                    break;
+                default:
+                    break;
             }
         }
     }
